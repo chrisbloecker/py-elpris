6788b923a64ba77407ff99b17385414a1f67467f
Christopher Blöcker <mail@chrisbloecker.se>
2022-12-31 15:40:20 +0100

    also tomorrow's prices

--- py_elpris/__init__.py
+++ py_elpris/__init__.py
@@ -1,7 +1,12 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-def get_prices():
+
+url = "https://elen.nu"
+
+
+def get_prices_today():
     """
     Retrieves today's electricity prices from elen.nu.
 
@@ -11,16 +16,35 @@ def get_prices():
     Returns:
         A dictionary with areas as keys and prices as values.
     """
+    soup   = BeautifulSoup(requests.get(url).text, "html.parser")
+    areas  = [ area.text for area in soup.find_all("div",  attrs = { "class" : "uppercase text-sm" }) ]
+    today  = [ t.text    for t    in soup.find_all("span", attrs = { "class" : "text-lg" }) ]
 
-    url      = "https://elen.nu"
-    content  = requests.get(url).text
-    soup     = BeautifulSoup(content, "html.parser")
-    areas    = soup.find_all("div",  attrs = { "class" : "uppercase text-sm" })
-    today    = soup.find_all("span", attrs = { "class" : "text-lg" })
-    tomorrow = soup.find_all("div",  attrs = { "class" : "text-xs" })
+    prices = { area : float(t.replace(",",".")) 
+                 for (area, t) in zip(areas, today)
+             }
+    
+    return prices
 
-    prices   = { area.text : float(t.text.replace(",",".")) 
-                   for (area, t) in zip(areas, today)
-               }
+
+def get_prices_tomorrow():
+    """
+    Retrieves tomorrow's electricity prices from elen.nu.
+
+    Raises:
+        ToDo: figure out which exceptions can be thrown
+
+    Returns:
+        A dictionary with areas as keys and prices as values.
+    """
+    soup     = BeautifulSoup(requests.get(url).text, "html.parser")
+    areas    = [ area.text for area in soup.find_all("div", attrs = { "class" : "uppercase text-sm" }) ]
+    tomorrow = [ t.text    for t    in soup.find_all("div", attrs = { "class" : "text-xs" }) ]
+
+    prices = { area : None for area in areas}
+    for (area, t) in zip(areas, tomorrow):
+        matches = re.findall(r"\d+,\d+", t)
+        if len(matches) == 1:
+            prices[area] = float(matches[0].replace(",","."))
     
     return prices
